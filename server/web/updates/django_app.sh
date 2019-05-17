cdd88de0795d270020ab592606cb3b428e0ce7e8
Tom Brouwer <tombrouwer@outlook.com>
2019-05-17 20:30:08 +0200

    FIXED: Django_app update script now updates static files also

--- server/web/updates/django_app.sh
+++ server/web/updates/django_app.sh
@@ -9,5 +9,10 @@ set -e  # Make sure errors will stop execution
 # Pull newest code on active branch
 git -C $REPO_DIR/datacatalog-frontend pull
 
+# Update the static files on the machine
+source $VENV_DIR/opendal_frontend/bin/activate
+python $REPO_DIR/datacatalog-frontend/django/datacatalog/manage.py collectstatic
+deactivate
+
 # Restart UWSGI for changes to take effect
 systemctl restart uwsgi
