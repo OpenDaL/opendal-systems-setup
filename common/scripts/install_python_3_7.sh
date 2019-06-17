80c505a2554ab1b64e8acc94f9de32cfa936a419
Tom Brouwer <tombrouwer@outlook.com>
2019-06-17 13:38:53 +0200

    Minor FIXES:
    * FIXED: python3-venv required in addition to python3.7-venv
    * FIXED: X-robots-tag=None header set in NGINX production config

--- common/scripts/install_python_3_7.sh
+++ common/scripts/install_python_3_7.sh
@@ -2,7 +2,7 @@
 set -e  # Make sure errors will stop execution
 
 # The script assumes apt update has been run already
-add-apt-repository ppa:deadsnakes/ppa
+add-apt-repository -y ppa:deadsnakes/ppa
 apt install -y python3.7
 apt install -y python3.7-dev
 apt install -y python3-venv #Required, otherwise venvs may not work with below package only
