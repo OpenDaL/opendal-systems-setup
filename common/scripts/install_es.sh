4c79f005654c8bc1f5e3336298e33fd65198086d
Tom Brouwer <tombrouwer@outlook.com>
2019-08-22 11:25:18 +0200

    es_database: Upgrade ES version to 7.2.1

--- common/scripts/install_es.sh
+++ common/scripts/install_es.sh
@@ -2,6 +2,6 @@
 set -e  # Make sure errors will stop execution
 
 
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.0.1-amd64.deb -P ~/temp
-dpkg -i ~/temp/elasticsearch-7.0.1-amd64.deb
-rm ~/temp/elasticsearch-7.0.1-amd64.deb
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.2.1-amd64.deb -P ~/temp
+dpkg -i ~/temp/elasticsearch-7.2.1-amd64.deb
+rm ~/temp/elasticsearch-7.2.1-amd64.deb
